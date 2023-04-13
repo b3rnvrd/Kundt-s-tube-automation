a3ae868784c03b1b377269997197850db90c4d59
b3rnvrd <eliottcolson.snir@gmail.com>
2023-04-13 14:06:49 +0200

    azy

--- arduino/pilotage_final/pilote_arduino/pilotage_ecran.cpp
+++ arduino/pilotage_final/pilote_arduino/pilotage_ecran.cpp
@@ -6,33 +6,9 @@ Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC);
 #define SD_CS 4
 
 void setupEcran() {
-  //  Serial.begin(9600);
-
   tft.begin();
-
   tft.setRotation(3);
   tft.fillScreen(ILI9341_BLACK);
-
-  // read diagnostics (optional but can help debug problems)
-  uint8_t x = tft.readcommand8(ILI9341_RDMODE);
-  //  Serial.print("Display Power Mode: 0x"); Serial.println(x, HEX);
-  //  x = tft.readcommand8(ILI9341_RDMADCTL);
-  //  Serial.print("MADCTL Mode: 0x"); Serial.println(x, HEX);
-  //  x = tft.readcommand8(ILI9341_RDPIXFMT);
-  //  Serial.print("Pixel Format: 0x"); Serial.println(x, HEX);
-  //  x = tft.readcommand8(ILI9341_RDIMGFMT);
-  //  Serial.print("Image Format: 0x"); Serial.println(x, HEX);
-  //  x = tft.readcommand8(ILI9341_RDSELFDIAG);
-  //  Serial.print("Self Diagnostic: 0x"); Serial.println(x, HEX);
-  //
-  //  Serial.println(F("Benchmark                Time (microseconds)"));
-  //  delay(10);
-  //
-  //  Serial.print(F("Text                     "));
-  //  delay(3000);
-
-
-
 }
 
 unsigned long affichageTexte(bool texte_stop = false) {
